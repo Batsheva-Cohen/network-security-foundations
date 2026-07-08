12947f79f4d8707a3cc499583045e4ea3299e2de
Batsheva <bat7cn@gmail.com>
2026-07-08 09:51:54 +0300

    refactor: rename create_token to generate_token

--- tests/test_decode_jwt.py
+++ tests/test_decode_jwt.py
@@ -10,7 +10,7 @@ def test_valid_secret():
     decoded_data = jwt.decode(token, VALID_SECRET, algorithms=["HS256"])
     assert decoded_data["sub"] == "dana"
 
-def test_in_valid_secret():
+def test_invalid_secret_raises_signature_error():
     INVALID_SECRET = "i.am.invalid.secret"
     VALID_SECRET = os.environ.get("MY_SECRET_KEY")
     token = decode_jwt.create_token(INVALID_SECRET)
